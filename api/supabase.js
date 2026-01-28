b8fe6f5b54590d6c308ab083f05ba48b501e9ed5
fragass <fkp9wx6y@gmail.com>
2026-01-27 21:00:06 -0300

    Delete api directory

--- api/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-
-export const supabase = createClient(
-import.meta.env.VITE_SUPABASE_URL,
-import.meta.env.VITE_SUPABASE_ANON_KEY
-)
